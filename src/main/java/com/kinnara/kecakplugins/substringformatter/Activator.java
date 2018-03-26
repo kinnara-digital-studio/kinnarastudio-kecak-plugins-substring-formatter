a8cb686a122f020ece7541887a8aa438486b5c56
aristo_sh <aristo.hadisoeganda@gmail.com>
2018-03-26 15:49:51 +0700

    release candidate

--- src/main/java/com/kinnara/kecakplugins/substringformatter/Activator.java
+++ src/main/java/com/kinnara/kecakplugins/substringformatter/Activator.java
@@ -14,7 +14,7 @@ public class Activator implements BundleActivator {
         registrationList = new ArrayList<ServiceRegistration>();
 
         //Register plugin here
-        //registrationList.add(context.registerService(MyPlugin.class.getName(), new MyPlugin(), null));
+        registrationList.add(context.registerService(SubStringFormatter.class.getName(), new SubStringFormatter(), null));
     }
 
     public void stop(BundleContext context) {
---
+++ src/main/java/com/kinnara/kecakplugins/substringformatter/SubStringFormatter.java
@@ -0,0 +1,88 @@
+package com.kinnara.kecakplugins.substringformatter;
+
+import org.joget.apps.app.service.AppPluginUtil;
+import org.joget.apps.app.service.AppUtil;
+import org.joget.apps.datalist.model.DataList;
+import org.joget.apps.datalist.model.DataListColumn;
+import org.joget.apps.datalist.model.DataListColumnFormatDefault;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SubStringFormatter extends DataListColumnFormatDefault {
+    @Override
+    public String format(DataList dataList, DataListColumn column, Object row, Object value) {
+        String result = value == null ? "" : value.toString();
+
+        try {
+            if (!getPropertyString("regex").isEmpty())
+                result = byPattern(result);
+
+            int start, end;
+            try {
+                start = Integer.valueOf(getPropertyString("start"));
+            } catch (NumberFormatException e) {
+                start = 0;
+            }
+
+            try {
+                end = Integer.valueOf(getPropertyString("end"));
+            } catch (NumberFormatException e) {
+                end = result.length();
+            }
+
+            return result.substring(start, end);
+        } catch (Exception e) {
+            return result;
+        }
+    }
+
+    private String byPattern(String value) {
+        int sequence;
+        try {
+            sequence = Integer.valueOf(getPropertyString("sequence"));
+        }catch (NumberFormatException e) {
+            sequence = 0;
+        }
+
+        Pattern p = Pattern.compile(getPropertyString("regex"));
+        Matcher m = p.matcher(value == null ? "" : value);
+
+        for(int i = 0; m.find(); i++) {
+            if(i == sequence)
+                return m.group();
+        }
+
+        return value;
+    }
+
+    @Override
+    public String getName() {
+        return AppPluginUtil.getMessage("subStringFormatter.title", getClassName(), "/messages/SubStringFormatter");
+    }
+
+    @Override
+    public String getVersion() {
+        return getClass().getPackage().getImplementationVersion();
+    }
+
+    @Override
+    public String getDescription() {
+        return getClass().getPackage().getImplementationTitle();
+    }
+
+    @Override
+    public String getLabel() {
+        return getName();
+    }
+
+    @Override
+    public String getClassName() {
+        return getClass().getName();
+    }
+
+    @Override
+    public String getPropertyOptions() {
+        return AppUtil.readPluginResource(getClassName(), "/properties/SubStringFormatter.json", null, false, "/messages/SubStringFormatter");
+    }
+}
